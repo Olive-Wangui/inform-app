9bb7ff971e11d74849865cf500e2f5c5e9ee501f
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-10-31 20:08:17 +0300

    Created an updateUser and updateProfile form for the profile section.

---
+++ inform/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import *
+ 
+ 
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+        
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'location', 'profile_picture']
+        
