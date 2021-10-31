27051fec60ed5e5477beb82ec703df2d5287cc43
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-10-31 12:39:44 +0300

    Applied CRUD in the neighbourhood model.

--- inform/models.py
+++ inform/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime as dt
+from django.http import Http404
 
 # Create your models here.
 class Neighbourhood(models.Model):
@@ -8,3 +10,44 @@ class Neighbourhood(models.Model):
     occupants_count = models.IntegerField()
     pub_date = models.DateTimeField(auto_now_add=True)
     Admin = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    
+    def save_neighbourhood(self):
+        self.save()
+    
+    def delete_neighbourhood(self):
+        self.delete()
+        
+    @classmethod
+    def get_neighbourhoods(cls):
+        projects = cls.objects.all()
+        return projects
+    
+    @classmethod
+    def search_neighbourhoods(cls, search_term):
+        projects = cls.objects.filter(name__icontains=search_term)
+        return projects
+    
+    
+    @classmethod
+    def get_by_admin(cls, Admin):
+        projects = cls.objects.filter(Admin=Admin)
+        return projects
+    
+    
+    @classmethod
+    def get_neighborhood(request, neighborhood):
+        try:
+            project = Neighbourhood.objects.get(pk = id)
+            
+        except ObjectDoesNotExist:
+            raise Http404()
+        
+        return project
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'My Neighborhood'
+        verbose_name_plural = 'Neighborhoods'
